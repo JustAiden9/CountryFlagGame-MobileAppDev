49873da50a7e51769ea09f35ca737335a9732db1
Aiden Baker <aidenbak9@gmail.com>
2025-01-08 10:09:57 -0600

    fixing quiestionview

--- Country Flag Game/Views/QuestionView.swift
+++ Country Flag Game/Views/QuestionView.swift
@@ -8,37 +8,46 @@
 import SwiftUI
 
 struct QuestionView: View {
+    @EnvironmentObject var gameManager: GameManager
     var body: some View {
-        VStack(spacing: 20) {
-            HStack {
+        if gameManager.playingGame {
+            VStack(spacing: 20) {
+                HStack {
+                    
+                    Text("Country Flag Game")
+                        .foregroundColor(.yellow)
+                        .fontWeight(.heavy)
+                    
+                    Spacer()
+                    
+                    Text("\(gameManager.index) out of \(gameManager.questions.count)")
+                        .foregroundColor(.yellow)
+                    
+                }
+                ProgressBar(progress: gameManager.progress)
                 
-                Text("Country Flag Game")
-                    .foregroundColor(.yellow)
-                    .fontWeight(.heavy)
-                
+                VStack(spacing: 10) {
+                    Text("Which country's flag is this?")
+                    Image(gameManager.country)
+                        .resizable()
+                        .frame(width: 300, height: 200)
+                    ForEach(gameManager.answerChoices) { answer in
+                        AnswerRow(answer: answer)
+                            .environmentObject(gameManager)
+                    }
+                }
+                Button {
+                    gameManager.goToNextQuestion()
+                } label: {
+                    CustomButton(text: "Next", background: gameManager.answerSelected ? .yellow : .gray)
+                }
+                .disabled(!gameManager.answerSelected)
+
                 Spacer()
-                
-                Text("1 out of 3")
-                    .foregroundColor(.yellow)
-                
             }
-            ProgressBar(progress: 50)
-            
-            VStack(spacing: 10) {
-                Text("Which country's flag is this?")
-                Image("Italy")
-                    .resizable()
-                    .frame(width: 300, height: 200)
-                AnswerRow(answer: Answer(text: "France", isCorrect: false))
-                AnswerRow(answer: Answer(text: "Germany", isCorrect: false))
-                AnswerRow(answer: Answer(text: "Italy", isCorrect: true))
-                AnswerRow(answer: Answer(text: "England", isCorrect: false))
-            }
-            CustomButton(text: "Next")
-            Spacer()
+            .padding()
+            .background(.cyan)
         }
-        .padding()
-        .background(.cyan)
     }
 }
 
